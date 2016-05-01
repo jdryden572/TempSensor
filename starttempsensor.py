389fded4286b20ac8cc4818cd5940f4601c4b32f
James Dryden <jdryden572@gmail.com>
2016-05-01 18:43:11 +0000

    Fixed client_address to reflect change to http.server.BasTeHTTPRequestHandler.client_address to be a tuble (host, port) https://docs.python.org/3/library/http.server.html

--- starttempsensor.py
+++ starttempsensor.py
@@ -9,16 +9,14 @@ class WebHandler(http.server.BaseHTTPRequestHandler):
 	writeFile = '/opt/LightingSystem/webServer/TempReadings.txt'
 	sensor = ReadTemp.initializeSensor()
 	def do_GET(self):
-		print('Received {0:s} request from {1:s}'.format(
-				self.command, self.client_address))
+		print('Received {0:s} request from {1:s}'.format(self.command, self.client_address[0]))
 		if self.path.endswith('/get_temp'):
 			self.send_response(200)
 			self.send_header('Content-type','text/html')
 			self.end_headers()
 			[tempC, tempF] = self.sensor.temp
 			timeString = time.strftime("%m/%d/%Y %H:%M:%S", time.localtime())
-			retString = ('{0:s} tempC={1:f}, tempF={2:f}'.format(
-								timeString, tempC, tempF))
+			retString = ('{0:s} tempC={1:f}, tempF={2:f}'.format(timeString, tempC, tempF))
 			print("Response: <{0}>".format(retString))
 			self.wfile.write(retString.encode('utf-8'))
 	
